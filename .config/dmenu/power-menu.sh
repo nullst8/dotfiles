94e104d10fb7da377226b1a57002ce904d92dda6
csh4dow <csh4dow@protonmail.com>
2018-01-01 05:55:40 +0530

    update ::

--- .config/dmenu/power-menu.sh
+++ .config/dmenu/power-menu.sh
@@ -3,17 +3,17 @@
 export LANG="en_US.utf8"
 
 # case "$(echo -e "Shutdown\nRestart\nLogout\nSuspend\nLock" | dmenu \
-case "$(echo -e "Shutdown\nRestart\nSuspend\nLock" | dmenu \
+case "$(echo "Shutdown\nRestart\nSuspend\nLock" | dmenu \
     -nb "${COLOR_BACKGROUND:-#151515}" \
     -nf "${COLOR_DEFAULT:-#aaaaaa}" \
     -sf "${COLOR_HIGHLIGHT:-#589cc5}" \
     -sb "#1a1a1a" \
     -i -p \
     "Power:" -l 5)" in
-        Shutdown) exec systemctl poweroff;;
-        Restart) exec systemctl reboot;;
+        Shutdown) exec loginctl poweroff;;
+        Restart) exec loginctl reboot;;
         # Logout) exec bspc quit;;
-        Suspend) exec systemctl suspend;;
+        Suspend) exec loginctl suspend;;
         Lock) exec slock;;
         # Lock) exec systemctl --user start lock.target;;
 esac
--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,7 @@
 picom --fade-in-step=1 --fade-out-step=1 --fade-delta=0 &
-feh --bg-fill /usr/share/backgrounds/solus/Excl_Autumn_Leaf.jpg
+feh --bg-fill /usr/share/backgrounds/xfce/xfce-blue.jpg
+
+brightnessctl s 100%
 
 dunst &
 
@@ -9,7 +11,7 @@ blueman-applet &
 xsetroot -cursor_name left_ptr &
 slstatus &
 
-# xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Left Handed Enabled" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
 xinput set-prop "HID 1bcf:08a0 Mouse" "libinput Left Handed Enabled" 1
@@ -24,4 +26,5 @@ redshift -x
 redshift -O 5000
 
 kdeconnect-indicator &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/libexec/polkit-gnome-authentication-agent-1 &
